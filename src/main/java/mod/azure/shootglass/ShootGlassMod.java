5fc82d5ba56f9326c1984c81c83218946a4c8a0d
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-08-25 08:27:33 -0400

    Forge port

--- src/main/java/mod/azure/shootglass/ShootGlassMod.java
+++ src/main/java/mod/azure/shootglass/ShootGlassMod.java
@@ -1,16 +1,16 @@
 package mod.azure.shootglass;
 
-import net.fabricmc.api.ModInitializer;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
+import net.minecraftforge.fml.common.Mod;
 
-public class ShootGlassMod implements ModInitializer {
+@Mod("shootglass")
+public class ShootGlassMod {
 
 	public static final TagKey<Block> BREAKABLE_BLOCKS = TagKey.create(Registries.BLOCK, new ResourceLocation("shootglass", "breakable_blocks"));
 
-	@Override
-	public void onInitialize() {
+	public ShootGlassMod() {
 	}
 }
