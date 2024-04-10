0b24b416987ee4987b9d328be13b473bac079b8e
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-04-09 20:05:57 -0400

    Move Mixin to common/Implement config option for breaking glass

--- common/src/main/java/mod/azure/shootglass/mixins/ProjectileMixin.java
+++ common/src/main/java/mod/azure/shootglass/mixins/ProjectileMixin.java
@@ -1,9 +1,9 @@
 package mod.azure.shootglass.mixins;
 
 import mod.azure.shootglass.ShootGlass;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.projectile.AbstractArrow;
-import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,17 +12,17 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AbstractArrow.class)
-public abstract class ProjectileMixin extends Projectile {
+public abstract class ProjectileMixin extends Entity {
 
-    public ProjectileMixin(EntityType<? extends Projectile> entityType, Level level) {
+    protected ProjectileMixin(EntityType<?> entityType, Level level) {
         super(entityType, level);
     }
 
     @Inject(at = @At("HEAD"), method = "onHitBlock")
-    private void breakglass(BlockHitResult blockHitResult, CallbackInfo info) {
+    private void breakGlass(BlockHitResult blockHitResult, CallbackInfo info) {
         if (level().getBlockState(blockHitResult.getBlockPos()).is(ShootGlass.BREAKABLE_BLOCKS)) {
             level().destroyBlock(blockHitResult.getBlockPos(), true);
-            if (!this.level().isClientSide)
+            if (!this.level().isClientSide && ShootGlass.shootGlassConfig.removeProjectile)
                 this.remove(RemovalReason.DISCARDED);
         }
     }
