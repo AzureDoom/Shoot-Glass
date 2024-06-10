5ba977fea7a53a6e449f94968a46ca35d9711a99
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-06-10 18:25:05 -0400

    1.20.6

--- common/src/main/java/mod/azure/shootglass/ShootGlassConfig.java
+++ common/src/main/java/mod/azure/shootglass/ShootGlassConfig.java
@@ -1,7 +1,7 @@
 package mod.azure.shootglass;
 
-import mod.azure.azurelib.config.Config;
-import mod.azure.azurelib.config.Configurable;
+import mod.azure.azurelib.common.api.common.config.Config;
+import mod.azure.azurelib.common.internal.common.config.Configurable;
 
 @Config(id = ShootGlass.MOD_ID)
 public class ShootGlassConfig {
